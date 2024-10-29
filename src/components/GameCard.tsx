e86caedcb1e27ff576509583c2e93e3b2f7882e8
Anushaka23 <anushaka.underpinservices@gmail.com>
2024-10-29 12:52:10 +0530

    adjust carousel

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -6,7 +6,7 @@ import GameBorder from "./svg/GameBorder";
 const GameCard = ({ position, data }: any) => {
   return (
     <div className="w-full h-full relative flex items-center justify-center perspective">
-      {position === 2 && (
+      {/* {position === 2 && (
         <div className="h-[110%] absolute w-[105%]">
           <Image
             src="/card-animation.gif"
@@ -18,7 +18,7 @@ const GameCard = ({ position, data }: any) => {
             className="z-[1] blur-[1.5px]"
           />
         </div>
-      )}
+      )} */}
       <GameBorder className="h-full w-full mx-auto z-[2]" />
       <Link
         href={`/game/${data?.slug}`}
