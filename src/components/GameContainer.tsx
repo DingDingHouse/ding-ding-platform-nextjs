a524ebe803daf1eb40005da69caa145893f3b273
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-11-07 11:29:26 +0530

    Image Optimzation

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -23,15 +23,14 @@ const GameContainer = ({
           className={`flex-none relative w-[20%] transition-all`}
         >
           {position === 2 && (
-            <div className="absolute top-0 sm:block hidden left-0 w-full  scale-110 h-full">
+            <div className="absolute top-0 sm:block hidden left-0 w-full  scale-[1.2] h-full">
               <Image
-                src="/card-animation.gif"
+                src="/card.gif"
                 alt=""
                 quality={100}
                 priority
                 height={4000}
                 width={4000}
-              
                 className={`z-[99]`}
               />
             </div>
